89dce080a4d85a71804c3270aad9fed17cb91515
Sébastien DA ROCHA <sebastien@da-rocha.net>
2018-02-06 15:58:41 +0100

    Added Geospatial extension

--- ckan/files/bootstrap.sh
+++ ckan/files/bootstrap.sh
@@ -2,7 +2,7 @@
 
 . /usr/lib/ckan/default/bin/activate
 
-cd /usr/lib/ckan/default/src/ckan
+cd /ckan
 
 # Création de la base par défaut
 paster db init -c /etc/ckan/default/production.ini
@@ -12,4 +12,4 @@ echo "CREATE USER ckan_default" | PGPASSWORD=pass psql -h db_datastore -U datast
 paster --plugin=ckan datastore set-permissions -c /etc/ckan/default/production.ini | PGPASSWORD=pass psql -h db_datastore -U datastore_default
 
 # Création de la base pour ckanext-spatial
-#paster --plugin=ckanext-spatial spatial initdb 4326 --config=/etc/ckan/default/production.ini
+paster --plugin=ckanext-spatial spatial initdb 4326 --config=/etc/ckan/default/production.ini
