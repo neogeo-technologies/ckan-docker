d9e6cf0728c344495c8f76b53871d3fbc3292d01
Sébastien DA ROCHA <sebastien@da-rocha.net>
2018-02-06 15:03:26 +0100

    Add extensions datapusher and datastore

--- ckan/files/bootstrap.sh
+++ ckan/files/bootstrap.sh
@@ -3,4 +3,13 @@
 . /usr/lib/ckan/default/bin/activate
 
 cd /usr/lib/ckan/default/src/ckan
+
+# Création de la base par défaut
 paster db init -c /etc/ckan/default/production.ini
+
+# Création de la base pour data_store
+echo "CREATE USER ckan_default" | PGPASSWORD=pass psql -h db_datastore -U datastore_default
+paster --plugin=ckan datastore set-permissions -c /etc/ckan/default/production.ini | PGPASSWORD=pass psql -h db_datastore -U datastore_default
+
+# Création de la base pour ckanext-spatial
+#paster --plugin=ckanext-spatial spatial initdb 4326 --config=/etc/ckan/default/production.ini
