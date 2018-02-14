5f392bdd86fbbdf9c6a59524d3110bf171cc18f9
Sébastien DA ROCHA <sebastien@da-rocha.net>
2018-02-14 14:46:03 +0100

    fix xloader patch of DB

--- ckan/files/bootstrap.sh
+++ ckan/files/bootstrap.sh
@@ -10,6 +10,8 @@ cd /ckan
 echo "CREATE USER datastore_default WITH PASSWORD 'pass'" | PGPASSWORD=pass psql -U ckan_default -h db
 echo "CREATE DATABASE datastore_default" | PGPASSWORD=pass psql -U ckan_default -h db
 echo "GRANT ALL ON DATABASE datastore_default TO ckan_default" | PGPASSWORD=pass psql -U ckan_default -h db
+# Préparation de la base pour xloader
+PGPASSWORD=pass psql -U ckan_default -h db -d datastore_default -f /usr/lib/ckan/default/src/ckanext-xloader/full_text_function.sql
 
 # Création de la base par défaut
 paster db init -c /etc/ckan/default/production.ini
@@ -22,5 +24,3 @@ paster --plugin=ckan datastore set-permissions -c /etc/ckan/default/production.i
 # Création de la base pour ckanext-spatial
 paster --plugin=ckanext-spatial spatial initdb 4326 --config=/etc/ckan/default/production.ini
 
-# Préparation de la base pour xloader
-PGPASSWORD=pass psql -U ckan_default -h db -d datastore_default -f /usr/lib/ckan/default/src/ckanext-xloader/full_text_function.sql
